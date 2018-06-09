e88ae695238d2c5206b4768e0d9d7643b98e777f
dv <suchkov.dv@gmail.com>
2018-06-09 09:46:34 +0300

    temp commit to fix work

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,38 @@
+import whois
+import requests
+from datetime import datetime
+import argparse
+
 def load_urls4check(path):
-    pass
+    with open(path, 'r', encoding='UTF8') as file_handler:
+        print(file_handler.readlines())
+
 
 def is_server_respond_with_200(url):
-    pass
+    return requests.get(url)
 
 def get_domain_expiration_date(domain_name):
-    pass
+    w = whois.whois(domain_name)
+    return w.expiration_date.isoformat()
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-f', action='store',
+        dest='filepath',
+        help='Filepath to urls list',
+        required=True
+    )
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
-    pass
+    url = 'https://aaaservice.jet.su'
+    try:
+        load_urls4check(parse_arguments().filepath)
+    except (FileNotFoundError, UnicodeDecodeError) as read_urls_error:
+        exit("Can't read file with urls:\n{}".format(read_urls_error))
+    # print(get_domain_expiration_date(url))
+    # print(is_server_respond_with_200(url))
+#
